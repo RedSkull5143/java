b7c80f779d636b85cdd1c396de01698e2a3763a7
RedSkull <omshinde5143@gmail.com>
2024-05-25 20:52:50 +0530

    Data type

---
+++ BasicJavaPrograms/RightTypeRiddle.java
@@ -0,0 +1,17 @@
+package BasicJavaPrograms;
+
+public class RightTypeRiddle {
+    public static void main(String[] args) {
+        int number1=1;
+        double number2=2;
+        char letter='a';
+        boolean flag=true;
+        String name="om shinde";
+
+        System.out.println(number1+" is integer");
+        System.out.println(number2+" is double");
+        System.out.println(letter+" is char");
+        System.out.println(flag+" is boolean");
+        System.out.println(name+" is string");
+    }
+}
