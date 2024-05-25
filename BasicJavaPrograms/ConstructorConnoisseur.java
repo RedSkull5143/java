b910dd5d99e16e77440605777fae195a6fbbc6f3
RedSkull <omshinde5143@gmail.com>
2024-05-25 21:22:08 +0530

    Fix class declaration and constructor functionality in ConstructorConnoisseur
    - Moved 'number' class declaration as an inner class inside 'ConstructorConnoisseur' as per the problem statement
    - Updated class name to 'Number' to comply with Java naming conventions
    - Modified the constructor to directly print the integer value as specified in the problem statement
    - Demonstrated usage of the corrected constructor in the main method

--- BasicJavaPrograms/ConstructorConnoisseur.java
+++ BasicJavaPrograms/ConstructorConnoisseur.java
@@ -1,20 +1,21 @@
 package BasicJavaPrograms;
 
-class number{
-    int a;
-
-    public number(int input){
-        this.a=input;
-    }
-
-    public void print(){
-        System.out.println("Input is "+a);
-    }
-}
 public class ConstructorConnoisseur {
+
+    // Inner class declaration as per the problem statement
+    static class Number {
+        int a;
+
+        // Constructor to initialize the instance variable
+        public Number(int input) {
+            this.a = input;
+            // Printing the integer directly inside the constructor
+            System.out.println("Input is " + a);
+        }
+    }
+
     public static void main(String[] args) {
-        number number=new number(10);
-        System.out.println(number.a);
-        number.print();
+        // Instantiating Number class with an integer argument
+        Number number = new Number(10);
     }
 }
