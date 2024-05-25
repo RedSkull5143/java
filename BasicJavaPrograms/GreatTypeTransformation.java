2dde0ddc62132a5261577c909a3e5e7919a9335e
RedSkull <omshinde5143@gmail.com>
2024-05-25 21:02:07 +0530

    data type transformation

---
+++ BasicJavaPrograms/GreatTypeTransformation.java
@@ -0,0 +1,9 @@
+package BasicJavaPrograms;
+
+public class GreatTypeTransformation {
+    public static void main(String[] args) {
+        double number1=80.89;
+        int parsed_value=(int) number1;
+        System.out.println("The double value is "+number1+" and parsed int is "+parsed_value);
+    }
+}
