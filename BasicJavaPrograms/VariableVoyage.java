998cab92521b34f826f3b0d2f53b506a7a59853d
RedSkull <omshinde5143@gmail.com>
2024-05-25 20:55:47 +0530

    Addition of 2 variables

---
+++ BasicJavaPrograms/VariableVoyage.java
@@ -0,0 +1,8 @@
+package BasicJavaPrograms;
+
+public class VariableVoyage {
+    public static void main(String[] args) {
+        int a=10, b=20;
+        System.out.println("Addition of "+a+" and "+b+" is "+(a+b));
+    }
+}
