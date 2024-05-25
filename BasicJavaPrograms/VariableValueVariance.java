0baf1bcab497d401cc7f5110e05455f61830d6fd
RedSkull <omshinde5143@gmail.com>
2024-05-25 20:58:25 +0530

    Updating variable value

---
+++ BasicJavaPrograms/VariableValueVariance.java
@@ -0,0 +1,10 @@
+package BasicJavaPrograms;
+
+public class VariableValueVariance {
+    public static void main(String[] args) {
+        int a=10;
+        System.out.println("Initial value of variable a is - "+a);
+        a=15;
+        System.out.println("Updated value of variable a is - "+a);
+    }
+}
