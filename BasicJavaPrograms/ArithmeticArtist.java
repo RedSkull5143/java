cde3b059debe65953a864f37f025f3f3d506857e
RedSkull <omshinde5143@gmail.com>
2024-07-20 20:57:29 +0530

    Arithmetic Operations

---
+++ BasicJavaPrograms/ArithmeticArtist.java
@@ -0,0 +1,23 @@
+package BasicJavaPrograms;
+
+public class ArithmeticArtist {
+    public static void main(String[] args) {
+        // Declare and initialize two integer variables
+        int a = 10, b = 2;
+
+        // Perform and print the addition of a and b
+        System.out.println("Addition is: " + (a + b));
+
+        // Perform and print the subtraction of b from a
+        System.out.println("Subtraction is: " + (a - b));
+
+        // Perform and print the multiplication of a and b
+        System.out.println("Multiplication is: " + (a * b));
+
+        // Perform and print the division of a by b
+        System.out.println("Division is: " + (a / b));
+
+        // Perform and print the remainder of a divided by b
+        System.out.println("Remainder is: " + (a % b));
+    }
+}
