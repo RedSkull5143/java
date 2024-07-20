919e83175a1f9bde348dcb1743cb96bdc55afcea
RedSkull <omshinde5143@gmail.com>
2024-07-20 21:38:27 +0530

    arithemtic ops of doubles

---
+++ ControlStructuresAndBasicOperations/ArithmeticAce.java
@@ -0,0 +1,22 @@
+package ControlStructuresAndBasicOperations;
+
+public class ArithmeticAce {
+    public static void main(String[] args) {
+        double a=10.5;
+        double b= 2.5;
+        // Perform and print the addition of a and b
+        System.out.println("Addition is: " + (a + b));
+
+        // Perform and print the subtraction of b from a
+        System.out.println("Subtraction is: " + (a - b));
+
+        // Perform and print the multiplication of a and b
+        System.out.println("Multiplication is: " + (a * b));
+
+        // Perform and print the division of a by b
+        System.out.println("Division is: " + (a / b));
+
+        // Perform and print the remainder of a divided by b
+        System.out.println("Remainder is: " + (a % b));
+    }
+}
